313af09cca6599679617da99894e5294d22b61f2
povarna <povarna@adobe.com>
2026-02-03 20:42:33 +0200

    AOC 2021 Day04

--- 01-go-fundamentals/aoc/2021/day04/problem.go
+++ 01-go-fundamentals/aoc/2021/day04/problem.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/povarna/generative-ai-with-go/fundamentals/utils"
 )
 
 var inputFile = flag.String("inputFile", "input.txt", "Relative path to the input file")
@@ -22,9 +25,134 @@ func Run() {
 }
 
 func part1(input *string) int {
-	return 1
+	nums, boards := readInput(input)
+
+	for _, n := range nums {
+		for _, b := range boards {
+			didWin := b.pickNum(n)
+			if didWin {
+				// multiply score of winning board by number that was just called
+				return b.score() * n
+			}
+		}
+	}
+
+	return -1
 }
 
 func part2(input *string) int {
-	return 1
+	nums, boards := readInput(input)
+
+	lastWinningScore := -1
+	alreadyWon := map[int]bool{}
+	for _, n := range nums {
+		for bi, b := range boards {
+			if alreadyWon[bi] {
+				continue
+			}
+			didWin := b.pickNum(n)
+			if didWin {
+				// WHICH BOARD WINS LAST
+				lastWinningScore = b.score() * n
+
+				// mark board as already won
+				alreadyWon[bi] = true
+			}
+		}
+	}
+
+	return lastWinningScore
+
+}
+
+type BoardState struct {
+	board  [][]int
+	picked [][]bool
+}
+
+func (b *BoardState) pickNum(num int) bool {
+	for r, rows := range b.board {
+		for c, v := range rows {
+			if v == num {
+				b.picked[r][c] = true
+			}
+		}
+	}
+
+	for i := 0; i < len(b.board); i++ {
+		isFullRow, isFullCol := true, true
+		// board is square so this works fine, otherwise would need another pair of nested loops
+		for j := 0; j < len(b.board); j++ {
+			// check row at index i
+			if !b.picked[i][j] {
+				isFullRow = false
+			}
+			// check col at index j
+			if !b.picked[j][i] {
+				isFullCol = false
+			}
+		}
+		if isFullRow || isFullCol {
+			// returns true if is winning board
+			return true
+		}
+	}
+
+	// false for incomplete board
+	return false
+}
+
+func newBoardState(board [][]int) BoardState {
+	picked := make([][]bool, len(board))
+	for i := range picked {
+		picked[i] = make([]bool, len(board[0]))
+	}
+	return BoardState{
+		board:  board,
+		picked: picked,
+	}
+}
+
+func (b *BoardState) score() int {
+	var score int
+
+	for r, rows := range b.board {
+		for c, v := range rows {
+			// adds up all the non-picked/marked cells
+			if !b.picked[r][c] {
+				score += v
+			}
+		}
+	}
+
+	return score
+}
+
+func readInput(input *string) ([]int, []BoardState) {
+	lines := strings.Split(*input, "\n\n")
+
+	var nums []int
+	var boards []BoardState
+
+	for v := range strings.SplitSeq(lines[0], ",") {
+		nums = append(nums, utils.ToInt(strings.TrimSpace(v)))
+	}
+
+	for _, group := range lines[1:] {
+		var board [][]int
+		boardLines := strings.SplitSeq(strings.TrimSpace(group), "\n")
+
+		for line := range boardLines {
+			fields := strings.Fields(line)
+			var row []int
+			for _, f := range fields {
+				row = append(row, utils.ToInt(f))
+			}
+			board = append(board, row)
+		}
+
+		boards = append(boards, newBoardState(board))
+	}
+
+	return nums, boards
 }
--- 01-go-fundamentals/main.go
+++ 01-go-fundamentals/main.go
@@ -1,7 +1,7 @@
 package main
 
-import aoc2022day06 "github.com/povarna/generative-ai-with-go/fundamentals/aoc/2022/day06"
+import aoc2021day04 "github.com/povarna/generative-ai-with-go/fundamentals/aoc/2021/day04"
 
 func main() {
-	aoc2022day06.Run()
+	aoc2021day04.Run()
 }
