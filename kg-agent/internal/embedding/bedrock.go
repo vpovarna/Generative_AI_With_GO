c3d5cc66cfaf9fe01bfdb82057670d98931b2181
povarna <povarna@adobe.com>
2026-02-14 21:25:53 +0200

    Adding generate embedder bedrock client

---
+++ kg-agent/internal/embedding/bedrock.go
@@ -0,0 +1,73 @@
+package embedding
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
+	"github.com/rs/zerolog/log"
+)
+
+var titanEmbeddingModelID string = "amazon.titan-embed-text-v2:0"
+
+type BedrockEmbedder struct {
+	client  *bedrockruntime.Client
+	modelID string
+}
+
+type BedrockEmbedderRequest struct {
+	InputText string `json:"inputText"`
+}
+
+type BedrockEmbedderResponse struct {
+	Embedding           []float32 `json:"embedding"`
+	InputTextTokenCount int       `json:"inputTextTokenCount"`
+}
+
+func NewBedrockEmbedder(bedrockClient *bedrockruntime.Client) *BedrockEmbedder {
+	return &BedrockEmbedder{
+		client:  bedrockClient,
+		modelID: titanEmbeddingModelID,
+	}
+}
+
+func (e *BedrockEmbedder) GenerateEmbeddings(ctx context.Context, query string) ([]float32, error) {
+
+	payload := BedrockEmbedderRequest{
+		InputText: query,
+	}
+
+	bytes, err := json.Marshal(payload)
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to serialize user query")
+		return nil, err
+	}
+
+	response, err := e.client.InvokeModel(ctx, &bedrockruntime.InvokeModelInput{
+		Body:        bytes,
+		ModelId:     aws.String(e.modelID),
+		ContentType: aws.String("application/json"),
+		Accept:      aws.String("application/json"),
+	})
+
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to query embedding model")
+		return nil, err
+	}
+
+	var bedrockEmbedderResponse BedrockEmbedderResponse
+
+	err = json.Unmarshal(response.Body, &bedrockEmbedderResponse)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed embedder response")
+		return nil, err
+	}
+
+	return bedrockEmbedderResponse.Embedding, nil
+}
+
+func (e *BedrockEmbedder) GenerateBatchEmbeddings(ctx context.Context, queries []string) ([][]float32, error) {
+	// TODO: Implement batch embeddings
+	return nil, nil
+}
