be082bb83a435e9866075a1f9d0bcc03319c26e6
povarna <povarna@adobe.com>
2026-02-25 07:22:11 +0200

    Removing AOC part

--- go-fundamentals/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/povarna/generative-ai-with-go/fundamentals
-
-go 1.25.6
--- go-fundamentals/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/povarna/generative-ai-with-go/fundamentals/interview"
-)
-
-func main() {
-	interview.Run()
-}
